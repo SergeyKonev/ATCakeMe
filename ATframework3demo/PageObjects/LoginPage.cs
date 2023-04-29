23745782c0a07c6b42013bfec2e505a3807b1c2a
Sergey Konev <sergey.konev.ru@gmail.com>
2023-04-29 16:06:02 +0200

    LoginPage.cs: Login page added

---
+++ ATframework3demo/PageObjects/LoginPage.cs
@@ -0,0 +1,19 @@
+﻿using atFrameWork2.SeleniumFramework;
+using atFrameWork2.TestEntities;
+using OpenQA.Selenium;
+
+namespace ATframework3demo.PageObjects
+{
+    public class LoginPage
+    {
+        public MainPage LogIn(PortalInfo info) 
+        {
+            new WebItem("//input[@name=\"USER_LOGIN\"]", "Поле ввода логина").SendKeys(Keys.Control + "A");
+            new WebItem("//input[@name=\"USER_LOGIN\"]", "Поле ввода логина").SendKeys(Keys.Delete);
+            new WebItem("//input[@name=\"USER_LOGIN\"]", "Поле ввода логина").SendKeys(info.PortalAdmin.Login);
+            new WebItem("//input[@name=\"USER_PASSWORD\"]", "Поле ввода пароля").SendKeys(info.PortalAdmin.Password);
+            new WebItem("//input[@name=\"Login\"]", "Кнопка войти").Click();
+            return new MainPage();
+        }
+    }
+}
