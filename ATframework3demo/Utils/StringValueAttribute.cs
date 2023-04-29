815cc51d3a5406dc9ece531be98d9c1f784c0496
Alan Gaziev <alan.gaziev27@gmail.com>
2023-04-30 00:57:07 +0200

    StringValueAttribute.cs: Utility class for setting string enums

---
+++ ATframework3demo/Utils/StringValueAttribute.cs
@@ -0,0 +1,18 @@
+﻿namespace ATframework3demo.Utils;
+
+public class StringValueAttribute : System.Attribute
+{
+
+    private string _value;
+
+    public StringValueAttribute(string value)
+    {
+        _value = value;
+    }
+
+    public string Value
+    {
+        get { return _value; }
+    }
+
+}
