d6ce444608b29c9a3dfc7025e18dfff4111aecca
Sergey Konev <sergey.konev.ru@gmail.com>
2023-04-29 16:15:21 +0200

    Case_LogIn.cs: Created a test for user log in

---
+++ ATframework3demo/TestCases/Case_LogIn.cs
@@ -0,0 +1,26 @@
+﻿using atFrameWork2.BaseFramework;
+using atFrameWork2.BaseFramework.LogTools;
+using atFrameWork2.SeleniumFramework;
+using atFrameWork2.TestEntities;
+using ATframework3demo.PageObjects;
+
+namespace ATframework3demo.TestCases
+{
+    public class Case_LogIn : CaseCollectionBuilder
+    {
+        protected override List<TestCase> GetCases()
+        {
+            var caseCollection = new List<TestCase>();
+            caseCollection.Add(new TestCase("Проверка авторизации", (mainPage, info) => SendToAllByDefault(mainPage,info)));
+            return caseCollection;
+        }
+
+
+        void SendToAllByDefault(MainPage mainPage, PortalInfo info)
+        {
+            mainPage.EnterLoginPage().LogIn(info);
+            if (!mainPage.IsAuthorized())
+                Log.Error("Не появилась кнопка входа в профиль после авторизации");
+        }
+    }
+}
