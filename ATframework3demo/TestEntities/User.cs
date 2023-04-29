0005cbf3ecb54ff2bc4ac4764a083f1f522b4e37
Alan Gaziev <alan.gaziev27@gmail.com>
2023-04-29 17:13:00 +0200

    User.cs: added specified for service fields to user

--- ATframework3demo/TestEntities/User.cs
+++ ATframework3demo/TestEntities/User.cs
@@ -6,7 +6,16 @@ namespace atFrameWork2.TestEntities
 {
     public class User
     {
+        /// <summary>
+        /// Информация о пользователе
+        /// </summary>
         public string Login { get; set; }
         public string Password { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public Gender Gender { get; set; }
+        public string? AdditionalInfo { get; set; }
+        public string? City { get; set; }
     }
 }
