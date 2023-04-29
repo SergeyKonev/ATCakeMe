fa13461db9e6be17e24c38065399bfb1ffd5f164
Alan Gaziev <alan.gaziev27@gmail.com>
2023-04-30 00:56:07 +0200

    added enums for dropdown selecting + refactor prefix "E_" to enums

---
+++ ATframework3demo/TestEntities/E_Category.cs
@@ -0,0 +1,33 @@
+﻿using ATframework3demo.Utils;
+
+namespace atFrameWork2.TestEntities;
+
+public enum Category
+{
+    [StringValue("Выпечка")]
+    Baking,
+    [StringValue("Супы")]
+    Soups,
+    [StringValue("Салаты")]
+    Salads,
+    [StringValue("Горячее блюдо")]
+    HotDish,
+    [StringValue("Холодное блюдо")]
+    ColdDish,
+    [StringValue("Диетическое")]
+    Dietary,
+    [StringValue("Сытное")]
+    Satisfying,
+    [StringValue("Сладкое")]
+    Sweet,
+    [StringValue("Веганское")]
+    Vegan,
+    [StringValue("Мясное")]
+    Meat,
+    [StringValue("Напитки")]
+    Drinks,
+    [StringValue("Закуска")]
+    Snack,
+    [StringValue("Соусы")]
+    Sauces
+}
---
+++ ATframework3demo/TestEntities/E_Unit.cs
@@ -0,0 +1,29 @@
+﻿using ATframework3demo.Utils;
+
+namespace atFrameWork2.TestEntities;
+
+public enum Unit
+{
+    [StringValue("грамм")]
+    Gram,
+    [StringValue("зубчика")]
+    Сlove,
+    [StringValue("кг")]
+    Kg,
+    [StringValue("куска")]
+    Slice,
+    [StringValue("л")]
+    L,
+    [StringValue("мл")]
+    Ml,
+    [StringValue("по вкусу")]
+    ToTaste,
+    [StringValue("ст. ложки")]
+    Tablespoon,
+    [StringValue("стакан")]
+    Glass,
+    [StringValue("ч. ложки")]
+    Teaspoon,
+    [StringValue("штук")]
+    Piece
+}
