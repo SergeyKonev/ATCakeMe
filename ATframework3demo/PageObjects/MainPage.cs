1f2b02263a66b9cc1200bcb7bde24aa7b9475e2a
Sergey Konev <sergey.konev.ru@gmail.com>
2023-04-29 17:30:16 +0200

    MainPage.cs: methods were transferred to Header.cs

--- ATframework3demo/PageObjects/MainPage.cs
+++ ATframework3demo/PageObjects/MainPage.cs
@@ -6,43 +6,5 @@ namespace ATframework3demo.PageObjects
 {
     public class MainPage
     {
-        public LoginPage EnterLoginPage()
-        {
-            new WebItem("//img[@aria-controls=\"dropdown-menu4\"]", "Выпадающее меню").Hover();
-            new WebItem("//a[@href=\"/auth/\"]", "Кнопка LogIn").Click();
-            return new LoginPage();
-        }
-
-
-        public ProfilePage EnterProfile() 
-        {
-            new WebItem("//img[@aria-controls=\"dropdown-menu4\"]", "Выпадающее меню").Hover();
-            Thread.Sleep(500);
-            new WebItem("//a[@href=\"/profile/\"]", "Кнопка перехода в профиль").Click();
-            return new ProfilePage();
-        }
-
-
-        public bool IsAuthorized()
-        {
-            new WebItem("//img[@aria-controls=\"dropdown-menu4\"]", "Выпадающее меню").Hover();
-            Thread.Sleep(500);
-            return new WebItem("//a[@href=\"/profile/\"]", "Кнопка перехода в профиль").WaitElementDisplayed();
-        }
-
-
-        public RecipeCreationPage EnterRecipeCreationPage()
-        {
-            new WebItem("//a[@href=\"/recipe/create/\"]", "Кнопка добаления рецепта").Click();
-            return new RecipeCreationPage();
-        }
-
-
-        public void Logout()
-        {
-            new WebItem("//img[@aria-controls=\"dropdown-menu4\"]", "Выпадающее меню").Hover();
-            Thread.Sleep(500);
-            new WebItem("//a[@href=\"/logout/\"]", "Кнопка выхода из аккаунта").Click();
-        }
     }
 }
