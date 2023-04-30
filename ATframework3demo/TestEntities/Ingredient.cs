ee5bc6e6738800b17efafd408356e16adb06aa33
Sergey Konev <sergey.konev.ru@gmail.com>
2023-04-30 10:48:02 +0200

    Recipe.cs: added consturctor, fields maded nullable

--- ATframework3demo/TestEntities/Ingredient.cs
+++ ATframework3demo/TestEntities/Ingredient.cs
@@ -2,7 +2,7 @@
 
 public class Ingredient
 {
-    public int id { get; set; }
+    public int Id { get; set; }
     public int Amount { get; set; }
     public Unit Unit { get; set; }
     public string Name { get; set; }
