55ce81f8a2bf9fbf52c3a89528e6947becb53ca4
Sergey Konev <sergey.konev.ru@gmail.com>
2023-04-30 12:07:34 +0200

    Recipe.cs: added constructor, fields maded nullable

--- ATframework3demo/TestEntities/RecipeStep.cs
+++ ATframework3demo/TestEntities/RecipeStep.cs
@@ -2,7 +2,14 @@
 
 public class RecipeStep
 {
+    public RecipeStep(int id, string? image = null, string? description = null)
+    {
+        Id = id;
+        Image = image;
+        Description = description;
+    }
+
     public int Id { get; set; }
     public string? Image { get; set; }
-    public string Description { get; set; }
+    public string? Description { get; set; }
 }
