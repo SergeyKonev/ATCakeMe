c74958a4167bbc3f2b2aaf6130f11b1a1efe4faf
Sergey Konev <sergey.konev.ru@gmail.com>
2023-04-30 12:32:43 +0200

    RecipeCreationPage.cs: filling of main fields, steps adding

--- ATframework3demo/PageObjects/RecipeCreationPage.cs
+++ ATframework3demo/PageObjects/RecipeCreationPage.cs
@@ -1,47 +1,86 @@
-﻿namespace ATframework3demo.PageObjects
+﻿using atFrameWork2.SeleniumFramework;
+using atFrameWork2.TestEntities;
+
+namespace ATframework3demo.PageObjects
 {
     public class RecipeCreationPage
     {
-        public MainPage CreateRecipe()            
+        /// <summary>
+        /// Class of recipe creation page
+        /// </summary>
+        /// <returns></returns>
+
+        WebItem nameField = new WebItem("//input[@name=\"RECIPE_NAME\"]", "Поле ввода названия рецепта");
+        WebItem descriptionField = new WebItem("//textarea[@name=\"RECIPE_DESC\"]", "Поле ввода описания рецепта");
+        WebItem portionNumField = new WebItem("//input[@name=\"RECIPE_PORTION\"]", "Поле ввода количества порций");
+        WebItem cookingTimeField = new WebItem("//input[@name=\"RECIPE_TIME\"]", "Поле ввода времени приготовления рецепта");
+        WebItem caloriesField = new WebItem("//input[@name=\"RECIPE_CALORIES\"]", "Поле ввода количества калорий");
+        WebItem submitButton = new WebItem("//input[@name=\"addRecipe\"]", "Кнопка сохранения рецета");
+        WebItem addStepButton = new WebItem("//a[contains(@class,\"instruction-button\")]", "Кнопка добавления шага рецепта");
+        WebItem addIngredientButton = new WebItem("//a[@class=\"table-button button is-primary is-light\"]", "Кнопка добавления ингредиента");
+
+        /// <summary>
+        /// Filling information on recipe creation page according to recipe object
+        /// </summary>
+        /// <param name="recipe"></param>
+        /// <returns></returns>
+        public MainPage CreateRecipe(Recipe recipe)            
         {
+            nameField.SendKeys(recipe.Name ?? "");
+            descriptionField.SendKeys(recipe.Description ?? "");
+            portionNumField.SendKeys(recipe.PortionNum.ToString() ?? "");
+            cookingTimeField.SendKeys(recipe.CookTime.ToString() ?? "");
+            caloriesField.SendKeys(recipe.Calories.ToString() ?? "");
+
+            ///not implemented
+            /* this.AddImages(recipe.Images);
+            this.AddCategories(recipe.Categories);
+            this.AddIngredients(recipe.Ingredients);*/
+            ///
+            this.AddSteps(recipe.Steps);
+            Thread.Sleep(10000); // test
+            submitButton.Click();
             return new MainPage();
         }
 
-
-        public void WriteRecipeName(String name)
+        private void AddSteps(List<RecipeStep>? steps)
         {
+            if (steps != null)
+            {
+                new WebItem("//textarea[@name=\"RECIPE_INSTRUCTION[]\"]", "Шаг 1 Описание").SendKeys(steps[0].Description ?? "");
+                //new WebItem("//input[@name=\"RECIPE_INSTRUCTION_IMAGES[]\"]", "Шаг 1 Изображение").SendKeys(steps[0].Image ?? "");
+                for (int i = 1; i < steps.Count; i++)
+                {
+                    addStepButton.Click();
+                    //TODO добавление изображений
+                    new WebItem($"//div[@class=\"field update-instruction-delete-{i+1}\"]//child::textarea", $"Шаг {i+1} Описание").SendKeys(steps[i].Description ?? "");
+                }
+            }
         }
 
-
-        public void WriteDescription(String description) 
+        private void AddIngredients(List<Ingredient>? ingredients)
         {
+            if (ingredients != null)
+            {
+                /*new WebItem("//textarea[@name=\"RECIPE_INSTRUCTION[]\"]", "Шаг 1 Описание").SendKeys(ingredients[0].Name ?? "");
+                //new WebItem("//input[@name=\"RECIPE_INSTRUCTION_IMAGES[]\"]", "Шаг 1 Изображение").SendKeys(steps[0].Image ?? "");
+                for (int i = 1; i < steps.Count; i++)
+                {
+                    addStepButton.Click();
+                    //TODO добавление изображений
+                    new WebItem($"//div[@class=\"field update-instruction-delete-{i + 1}\"]//child::textarea", $"Шаг {i + 1} Описание").SendKeys(steps[i].Description ?? "");
+                }*/
+            }
         }
 
-
-        public void WriteNumOfPortions(String numOfPortions)
+        private void AddCategories(List<Category>? categories)
         {
+            throw new NotImplementedException();
         }
 
-
-        public void WriteCookingTime(String cookingTime)
+        private void AddImages(List<string>? images)
         {
+            throw new NotImplementedException();
         }
-
-
-        public void WriteCalories(String calories)
-        {
-        }
-
-
-        public void WriteIngredient(String ingredient)
-        {
-        }
-
-
-        public void WriteStep(String step)
-        {
-        }
-
-
     }
 }
