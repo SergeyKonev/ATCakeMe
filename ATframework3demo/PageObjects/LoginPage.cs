fd1af44e201ce16b5c89ea9c0a5d69f317686368
Sergey Konev <sergey.konev.ru@gmail.com>
2023-04-30 09:54:17 +0200

    LoginPage.cs: Changed structure of class, added description

--- ATframework3demo/PageObjects/LoginPage.cs
+++ ATframework3demo/PageObjects/LoginPage.cs
@@ -4,15 +4,27 @@ using OpenQA.Selenium;
 
 namespace ATframework3demo.PageObjects
 {
+    /// <summary>
+    /// Login Page Class
+    /// </summary>
     public class LoginPage
     {
+        WebItem loginField = new WebItem("//input[@name=\"USER_LOGIN\"]", "Поле ввода логина");
+        WebItem passwordField = new WebItem("//input[@name=\"USER_PASSWORD\"]", "Поле ввода пароля");
+        WebItem submitButton = new WebItem("//input[@name=\"Login\"]", "Кнопка войти");
+
+        /// <summary>
+        /// Login to account using test information
+        /// </summary>
+        /// <param name="info"></param>
+        /// <returns></returns>
         public MainPage LogIn(PortalInfo info) 
         {
-            new WebItem("//input[@name=\"USER_LOGIN\"]", "Поле ввода логина").SendKeys(Keys.Control + "A");
-            new WebItem("//input[@name=\"USER_LOGIN\"]", "Поле ввода логина").SendKeys(Keys.Delete);
-            new WebItem("//input[@name=\"USER_LOGIN\"]", "Поле ввода логина").SendKeys(info.PortalAdmin.Login);
-            new WebItem("//input[@name=\"USER_PASSWORD\"]", "Поле ввода пароля").SendKeys(info.PortalAdmin.Password);
-            new WebItem("//input[@name=\"Login\"]", "Кнопка войти").Click();
+            loginField.SendKeys(Keys.Control + "A");
+            loginField.SendKeys(Keys.Delete);
+            loginField.SendKeys(info.PortalAdmin.Login);
+            passwordField.SendKeys(info.PortalAdmin.Password);
+            submitButton.Click();
             return new MainPage();
         }
     }
