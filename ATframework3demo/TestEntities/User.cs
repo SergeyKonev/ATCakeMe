63e17eae70141f9f979d15bf878eb7ece72984f6
Alan Gaziev <alan.gaziev27@gmail.com>
2023-04-30 15:26:24 +0200

    User.cs: added constructor + display in log on create

--- ATframework3demo/TestEntities/User.cs
+++ ATframework3demo/TestEntities/User.cs
@@ -1,11 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using atFrameWork2.BaseFramework.LogTools;
 
 namespace atFrameWork2.TestEntities
 {
     public class User
     {
+        public User(string login, string password, 
+            string email, string firstName, string secondName, 
+            Gender gender, string? additionalInfo = null, string? city = null)
+        {
+            Login = login;
+            Password = password;
+            Email = email;
+            FirstName = firstName;
+            SecondName = secondName;
+            Gender = gender;
+            AdditionalInfo = additionalInfo;
+            City = city;
+            Log.Info($"Новый пользователь создан: Логин {login}," +
+                     $"Пароль {password}," +
+                     $"Email {email}," +
+                     $"Имя {firstName}," +
+                     $"Фамилия {secondName}," +
+                     $"Пол {gender}," +
+                     $"Дополнительная информация {additionalInfo}," +
+                     $"Город {city}");
+        }
+
         /// <summary>
         /// Информация о пользователе
         /// </summary>
