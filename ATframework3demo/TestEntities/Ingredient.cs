55d224cbffb2f085fab8b5c6284aaeec8ef2c5e1
Sergey Konev <sergey.konev.ru@gmail.com>
2023-04-30 12:06:32 +0200

    Ingredient.cs: added constructor, fields maded nullable

--- ATframework3demo/TestEntities/Ingredient.cs
+++ ATframework3demo/TestEntities/Ingredient.cs
@@ -2,8 +2,16 @@
 
 public class Ingredient
 {
+    public Ingredient(int id, int? amount = null, Unit? unit = null, string? name = null)
+    {
+        Id = id;
+        Amount = amount;
+        Unit = unit;
+        Name = name;
+    }
+
     public int Id { get; set; }
-    public int Amount { get; set; }
-    public Unit Unit { get; set; }
-    public string Name { get; set; }
+    public int? Amount { get; set; }
+    public Unit? Unit { get; set; }
+    public string? Name { get; set; }
 }
