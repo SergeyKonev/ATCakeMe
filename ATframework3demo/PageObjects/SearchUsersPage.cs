a40996bb70065c0156f3614c2111d6666c81d690
Sergey Konev <sergey.konev.ru@gmail.com>
2023-05-02 22:02:01 +0200

    SearchUsersPage.cs: added

---
+++ ATframework3demo/PageObjects/SearchUsersPage.cs
@@ -0,0 +1,16 @@
+﻿using atFrameWork2.SeleniumFramework;
+
+namespace ATframework3demo.PageObjects
+{
+    public class SearchUsersPage
+    {
+        public ProfilePage SearchForUser(string firstName, string secondName) 
+        {
+            new WebItem("//input[@name=\"search_name\"]", "Поле ввода имени").SendKeys(firstName);
+            new WebItem("//input[@name=\"search_last\"]", "Поле ввода фамилии").SendKeys(secondName);
+            new WebItem("//button[@type=\"submit\"]", "Кнопка поиска").Click();
+            new WebItem($"//strong[text() = \"{firstName} {secondName}\"]", "Карточка профиля").Click();
+            return new ProfilePage();
+        }
+    }
+}
