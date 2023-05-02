18939ca8d06d341339865222f7d5777dc1c81ec9
Alan Gaziev <alan.gaziev27@gmail.com>
2023-05-02 19:12:53 +0200

    Case_Profile.cs: add case collection with profile

---
+++ ATframework3demo/TestCases/Case_Profile.cs
@@ -0,0 +1,27 @@
+﻿using atFrameWork2.BaseFramework;
+using atFrameWork2.TestEntities;
+using ATframework3demo.PageObjects;
+
+namespace ATframework3demo.TestCases;
+
+public class Case_Profile : CaseCollectionBuilder
+{
+    protected override List<TestCase> GetCases()
+    {
+        var caseCollection = new List<TestCase>();
+        caseCollection.Add(new TestCase("Проверка изменения профиля", (mainPage, info) => EditProfile(mainPage, info)));
+        return caseCollection;
+    }
+
+    void EditProfile(MainPage mainPage, PortalInfo info)
+    {
+        // Входим в свой профиль
+        Header
+            .EnterLoginPage()
+            .LogIn(info.PortalAdmin);
+        var oldUser = Header
+            .EnterProfile()
+            .OpenProfileEditor()
+            .GetUserInfo();
+    }
+}
