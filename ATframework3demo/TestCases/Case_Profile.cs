c87e1c0bb734a03fa98a7a1e51ae795a019dbd7d
Alan Gaziev <alan.gaziev27@gmail.com>
2023-05-02 23:13:56 +0200

    Case_Profile.cs: end user data changing case + docs

--- ATframework3demo/TestCases/Case_Profile.cs
+++ ATframework3demo/TestCases/Case_Profile.cs
@@ -1,6 +1,8 @@
 ﻿using atFrameWork2.BaseFramework;
+using atFrameWork2.BaseFramework.LogTools;
 using atFrameWork2.TestEntities;
 using ATframework3demo.PageObjects;
+using ATframework3demo.Utils;
 
 namespace ATframework3demo.TestCases;
 
@@ -63,9 +65,35 @@ public class Case_Profile : CaseCollectionBuilder
         Header
             .EnterLoginPage()
             .LogIn(info.PortalAdmin);
-        var oldUser = Header
+        // Открываем профиль
+        var profileEditor = Header
             .EnterProfile()
-            .OpenProfileEditor()
-            .GetUserInfo();
+        // Открываем редактирование профиля
+            .OpenProfileEditor();
+        
+        // Сохраняем данные о пользователе
+        var oldUser = profileEditor.GetUserInfo();
+        
+        // Создаем объект нового пользователя
+        User newUser = new User(
+            login: Generator.RandomString(Generator.RandomInt(3, 10)),
+            password: Generator.RandomString(Generator.RandomInt(6, 15)),
+            email: Generator.RandomString(Generator.RandomInt(5, 10)) + "@mail.ru",
+            firstName: Generator.RandomString(Generator.RandomInt(1, 10)),
+            secondName: Generator.RandomString(Generator.RandomInt(1, 10)),
+            gender: Generator.RandomGender(),
+            additionalInfo: Generator.RandomString(15),
+            city: Generator.RandomString(10)
+        );
+
+        // Изменяем данные о пользователе
+        profileEditor
+            .EditUserProfileInfo(newUser)
+            .SaveEditedInfo();
+        
+        // Проверяем пользовательские данные
+        Header
+            .EnterProfile()
+            .CheckUser(newUser);
     }
 }
