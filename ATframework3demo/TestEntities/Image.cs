0d473f4aa6540b85d7599922fcf2c420fdcd3571
Alan Gaziev <alan.gaziev27@gmail.com>
2023-05-03 15:04:40 +0200

    Image.cs: implemented image class with path constructor

---
+++ ATframework3demo/TestEntities/Image.cs
@@ -0,0 +1,14 @@
+﻿namespace atFrameWork2.TestEntities;
+
+public class Image
+{
+    public string Name;
+    public string Path;
+
+    public Image(string name)
+    {
+        Name = name;
+        string workingDirectory = Environment.CurrentDirectory;
+        Path = Directory.GetParent(workingDirectory).Parent.Parent.FullName + $"\\src\\{name}";
+    }
+}
