e66df8c88105a212af4567fa438ba0f1eca946ea
Sergey Konev <sergey.konev.ru@gmail.com>
2023-05-03 16:36:39 +0200

    Case_Login.cs: comments added

--- ATframework3demo/TestCases/Case_LogIn.cs
+++ ATframework3demo/TestCases/Case_LogIn.cs
@@ -15,12 +15,32 @@ namespace ATframework3demo.TestCases
             return caseCollection;
         }
 
-
+        /// <summary>
+        /// Проверка авторизации пользователя, сравнение логина, указываемого при входе, с отображаемым в профиле
+        /// </summary>
+        /// <param name="mainPage"></param>
+        /// <param name="info"></param>
         void Login(MainPage mainPage, PortalInfo info)
         {
             Header.EnterLoginPage().LogIn(info.PortalAdmin);
             if (!Header.IsAuthorized())
                 Log.Error("Не появилась кнопка входа в профиль после авторизации");
+            User user = Header.EnterProfile().OpenProfileEditor().GetUserInfo();
+            if (user.Login != info.PortalAdmin.Login) 
+            {
+                Log.Error("У пользователя отображается неверный логин");
+            }
+        }
+
+        /// <summary>
+        /// NOT IMPLEMENTED
+        /// </summary>
+        /// <param name="mainPage"></param>
+        /// <param name="info"></param>
+        void LoginFromRegistrationPage(MainPage mainPage, PortalInfo info)
+        {
+            Header.EnterRegisterPage().EnterLoginPage();
+            
         }
     }
 }
