9ee67336fdda1de87168cce5b3a3f30c6e705b33
Alan Gaziev <alan.gaziev27@gmail.com>
2023-05-04 17:55:03 +0200

    Merge

---
+++ ATframework3demo/PageObjects/DetailPage.cs
@@ -0,0 +1,19 @@
+﻿using atFrameWork2.SeleniumFramework;
+using atFrameWork2.TestEntities;
+
+namespace ATframework3demo.PageObjects;
+
+public class DetailPage
+{
+    public Recipe GetRecipeInfo()
+    {
+        //return new Recipe();
+        return null;
+    }
+
+    public ProfilePage OpenAuthorProfilePage()
+    {
+        new WebItem("//p[@class=\"title is-4\"]/a", "Переход на страницу автора").Click();
+        return new ProfilePage();
+    }
+}
