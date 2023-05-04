4c3cc6804ee1651e9457139c21a0a61e5bd4ef5f
Alan Gaziev <alan.gaziev27@gmail.com>
2023-05-04 17:53:42 +0200

    Case_Redirect.cs: add two cases redirects from detail page + refactor

--- ATframework3demo/TestCases/Case_Redirect.cs
+++ ATframework3demo/TestCases/Case_Redirect.cs
@@ -12,8 +12,12 @@ namespace ATframework3demo.TestCases
         protected override List<TestCase> GetCases()
         {
             var caseCollection = new List<TestCase>();
-            caseCollection.Add(new TestCase("Проверка перехода из карточки чужого рецепта на автора", (mainPage, info) => AuthorFromCard(mainPage,info)));
-            caseCollection.Add(new TestCase("Проверка перехода из карточки своего рецепта на свою страницу", (mainPage, info) => OnMeFromCard(mainPage,info)));
+            
+            caseCollection.Add(new TestCase("Переход из карточки чужого рецепта на автора", (mainPage, info) => AuthorFromCard(mainPage,info)));
+            caseCollection.Add(new TestCase("Переход из карточки своего рецепта на свою страницу", (mainPage, info) => OnMeFromCard(mainPage,info)));
+            caseCollection.Add(new TestCase("Переход на страницу автора чужого рецепта из детальной страницы", (mainPage, info) => AuthorFromDetail(mainPage,info)));
+            caseCollection.Add(new TestCase("Переход на страницу профиля из детальной страницы своего рецепта", (mainPage, info) => OnMeFromDetail(mainPage,info)));
+
             return caseCollection;
         }
 
@@ -56,7 +60,7 @@ namespace ATframework3demo.TestCases
             Header
                 .SearchRecipeByName(recipe1.Name)
                 // Переходим по ссылке на автора этого рецепта
-                .openRecipeAuthorPage(recipe1.Name)
+                .OpenRecipeAuthorPage(recipe1.Name)
                 // Проверяем открылась страница автора или другая
                 .CheckUser(user1);
 
@@ -89,8 +93,85 @@ namespace ATframework3demo.TestCases
             Header
                 .SearchRecipeByName(recipe1.Name)
                 // Переходим по ссылке на автора этого рецепта
-                .openRecipeAuthorPage(recipe1.Name)
+                .OpenRecipeAuthorPage(recipe1.Name)
                 // Проверяем открылась страница автора или другая
+                .CheckUser(user1);
+        }
+
+        void AuthorFromDetail(MainPage mainPage, PortalInfo info)
+        {
+            // Логинимся в аккаунт
+            Header
+                .EnterLoginPage()
+                .LogIn(info.PortalAdmin);
+            if (!Header.IsAuthorized())
+                Log.Error("Не появилась кнопка входа в профиль после авторизации");
+
+            // Получаем информацию о пользователе, чтобы потом проверить её
+            var user1 = Header
+                .EnterProfile()
+                .OpenProfileEditor()
+                .GetUserInfo();
+            
+            // Создаем объект тестового рецепта
+            Recipe recipe1 = Generator.RandomRecipe();
+
+            // Создаем рецепт на сайте
+            Header
+                .EnterRecipeCreationPage()
+                .CreateRecipe(recipe1);
+            
+            // Выходим со страницы
+            Header
+                .Logout();
+            
+            // Создаем объект второго пользователя и регистрируем его
+            var user2 = Generator.RandomUser();
+            Header
+                .EnterRegisterPage()
+                .RegisterNewUser(user2)
+                .LogIn(user2);
+
+            // Находим созданный рецепт
+            Header
+                .SearchRecipeByName(recipe1.Name)
+                // Переходим по ссылке на автора этого рецепта
+                .OpenRecipeDetailPage(recipe1.Name)
+                // Проверяем открылась страница автора или другая
+                .OpenAuthorProfilePage()
+                .CheckUser(user1);
+        }
+        
+        void OnMeFromDetail(MainPage mainPage, PortalInfo info)
+        {
+            // Логинимся в аккаунт
+            Header
+                .EnterLoginPage()
+                .LogIn(info.PortalAdmin);
+            if (!Header.IsAuthorized())
+                Log.Error("Не появилась кнопка входа в профиль после авторизации");
+            
+            // Получаем информацию о пользователе, чтобы потом проверить её
+            var user1 = Header
+                .EnterProfile()
+                .OpenProfileEditor()
+                .GetUserInfo();
+            
+            // Создаем объект тестового рецепта
+            Recipe recipe1 = Generator.RandomRecipe();
+
+            // Создаем рецепт на сайте
+            Header
+                .EnterRecipeCreationPage()
+                .CreateRecipe(recipe1);
+            
+            // Находим созданный рецепт
+            Header
+                .SearchRecipeByName(recipe1.Name)
+                // Переходим по ссылке на автора этого рецепта
+                .OpenRecipeDetailPage(recipe1.Name)
+                // Проверяем открылась страница автора или другая
+                .OpenAuthorProfilePage()
                 .CheckUser(user1);
         }
     }
