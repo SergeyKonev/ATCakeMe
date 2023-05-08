80cc5d7182f05b31742998514814e2a96732e1a3
Sergey Konev <sergey.konev.ru@gmail.com>
2023-05-08 10:30:51 +0200

    SubscriptionPage.cs: added

---
+++ ATframework3demo/PageObjects/SubscriptionsPage.cs
@@ -0,0 +1,20 @@
+﻿using atFrameWork2.SeleniumFramework;
+using atFrameWork2.TestEntities;
+
+namespace ATframework3demo.PageObjects
+{
+    public class SubscriptionsPage
+    {
+
+        /// <summary>
+        /// Проверка отображения указанного пользователя в подписках
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        public bool IsSubscribed(User user)
+        {
+            return new WebItem($"//strong[text() = \"{user.FirstName} {user.SecondName}\"]", "Карточка пользователя").WaitElementDisplayed();
+        }
+        
+    }
+}
